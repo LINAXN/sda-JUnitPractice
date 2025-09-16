45dbf55ef95066819a30feee0cfa3ba93bd940d1
Lina Khaled <linaxn4@gmail.com>
2025-09-16 14:43:15 +0300

    Add ExcelUtils class for reading Excel files and retrieving cell data

--- src/test/java/utillties/ExcelUtils.java
+++ src/test/java/utillties/ExcelUtils.java
@@ -1,4 +1,27 @@
 package utillties;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.WebElement;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class ExcelUtils {
+    private Workbook workbook;
+
+    public ExcelUtils(String contentRoot){
+  try {
+      workbook= WorkbookFactory.create(new FileInputStream(contentRoot));
+  } catch (IOException e) {
+      throw new RuntimeException(e);
+  }
+    }
+
+    public Cell getcell(String sheet,int row , int cell){
+        return workbook.getSheet(sheet).getRow(row).getCell(cell);
+
+    }
+
 }
